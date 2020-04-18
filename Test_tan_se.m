50e07190147794b1c38cd588f109068acc26af85
0964wd <61693435+0964wd@users.noreply.github.com>
2020-04-18 18:09:37 +0800

    Add files via upload

---
+++ Test_tan_se.m
@@ -0,0 +1,15 @@
+function e=Test_tan_se()
+a=round(rand(1,1000)*360);%产生1000个0-360之间的数
+%b=tand(a);%以角度制计算tan函数，tand是MATLAB固有函数
+t=0;
+T=length(a);
+for i=1:T %比较，计算错误的个数
+    b(i)=tand(a(i));
+    c(i) = tan_se( a(i) );
+    if b(i)~=c(i)
+        t=t+1;
+    end
+end
+  erro=t/T;    
+  e=erro;
+end
